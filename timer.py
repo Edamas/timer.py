69f87952d5a29c43f7902f71dc652d9eb821e292
Elysio Neto <49333427+Edamas@users.noreply.github.com>
2021-03-08 05:54:22 -0300

    Create timer.py

---
+++ timer.py
@@ -0,0 +1,37 @@
+import datetime as dt
+
+clocks = '🕐🕑🕒🕓🕔🕕🕖🕗🕘🕙🕚🕛'
+
+def timer(mins):
+    '''
+    PRECISION TIMER 
+    
+    Once the timer function is called, 
+    
+    1- finish time is calculated as follows: 
+       
+        now + minutes = finish time
+    
+    2- Each loop recalculates:
+    
+        (remaining time - now) / remaining loops
+    
+    3- The process finishes with a little deviation
+    (less than using just 'time.sleep(minutes)' methods)
+    '''
+    
+    t1       = dt.datetime.now() + dt.timedelta(minutes=mins)
+    marks    = len(clocks)
+
+    print(clocks[-1], end='')
+    
+    for n in range(marks):
+        time.sleep((t1-dt.datetime.now()).total_seconds()/(marks-n))   # timer adjusted to remaining time
+        print(clocks[n], end='')
+    
+    print()
+    return 
+    
+    
+>> timer(60)
+🕛🕐🕑🕒🕓🕔🕕🕖🕗🕘🕙🕚🕛 # finishing after 60 seconds
